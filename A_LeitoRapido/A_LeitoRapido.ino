cfc02ad408da4d2929fb05d100ecbb1436a89ec9
marciojosebaldo <marciojosebaldo@gmail.com>
2023-09-19 19:14:57 -0300

    Add files via upload
    Pseudo hello world

---
+++ A_LeitoRapido/A_LeitoRapido.ino
@@ -0,0 +1,22 @@
+void setup() {
+
+  led = 13; // exclusiva para o LED
+
+  for (int i = 2; i <= 12; i++) {
+    pinMode(i, INPUT);
+  }
+
+  for (int j = 22; j <= 53; j++) {
+    pinMode(j, INPUT);
+  }
+
+  pinMode(led, OUTPUT);
+  digitalWrite(led, low);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
