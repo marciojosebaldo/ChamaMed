7b45cb321d777293d4bea4f77f819679b8fb6055
marciojosebaldo <marciojosebaldo@gmail.com>
2023-09-20 20:47:28 -0300

    Add files via upload

---
+++ J_ChamaMed/ChamaMed/src/main/java/ChamaMed/ChamaMed/ChamaMedApplication.java
@@ -0,0 +1,13 @@
+package ChamaMed.ChamaMed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamaMedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamaMedApplication.class, args);
+	}
+
+}
---
+++ J_ChamaMed/ChamaMed/src/test/java/ChamaMed/ChamaMed/ChamaMedApplicationTests.java
@@ -0,0 +1,13 @@
+package ChamaMed.ChamaMed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamaMedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
