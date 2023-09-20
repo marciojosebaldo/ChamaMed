7fd29e87c141a56d6bc304f81318b7b245a48d75
marciojosebaldo <marciojosebaldo@gmail.com>
2023-09-20 19:16:44 -0300

    Add files via upload

--- A_ChamaMed/A_ChamaMed.ino
+++ A_ChamaMed/A_ChamaMed.ino
@@ -2,28 +2,38 @@ void setup() {
 
   Serial.begin(9600);
   
+  bipe = 12; // pino exclusivo para o bipe
   led = 13; // pino exclusivo para o LED
+  
+  pinMode(led, OUTPUT);
+  pinode(bipe, OUTPUT);
+  
   digitalWrite(led, low);
+  digitalWrite(bipe, low);
 
-  for (int i = 2; i <= 12; i++) {
-    pinMode(i, INPUT);
+  for (int i = 2; i <= 11; i++) {
+    pinMode(porta[i], INPUT);
   }
 
   for (int j = 22; j <= 53; j++) {
-    pinMode(j, INPUT);
+    pinMode(porta[j], INPUT);
   }
 
-  pinMode(led, OUTPUT);
 }
 
 void loop() {
 
   if(Serial.available()) {
     for (int i = 2; i <= 12; i++) {
-    // Portas com booleano
+      if(porta[i] == HIGH) {
+        
+      }
   }
 
   for (int j = 22; j <= 53; j++) {
-    // Portas com booleano
+    if(porta[j] == HIGH) {
+
+    }
   }
+
 }
