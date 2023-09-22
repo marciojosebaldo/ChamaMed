bd868de1c06ac6e0b48c7792059cf84b541ae0b0
marciojosebaldo <marciojosebaldo@gmail.com>
2023-09-21 22:01:44 -0300

    Java

--- J_ChamaMed/ChamaMed/src/main/java/ChamaMed/ChamaMed/Service/S_Arduino.java
+++ J_ChamaMed/ChamaMed/src/main/java/ChamaMed/ChamaMed/Service/S_Arduino.java
@@ -1,6 +1,11 @@
 package ChamaMed.ChamaMed.Service;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import com.fazecast.jSerialComm.SerialPort;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class S_Arduino {
 
@@ -11,6 +16,21 @@ public class S_Arduino {
     public static void Arduino() {
         SerialPort serialPort = SerialPort.getCommPort(nomePortaArduino);
         serialPort.setBaudRate(baudRate);
+
+        if(!serialPort.openPort()) {
+            System.err.println("Falha ao conectar com a porta serial");
+            return;
+        }
+
+        InputStream inputStream = serialPort.getInputStream();
+        boolean[] buffer = new boolean[42]; // São 42 portas no Arduino
+        List<Boolean> portasArduino = new ArrayList<>();
+
+        while(true) {
+
+            try
+
+        }
 
 
     }
