e081c9bf2af5883a7a76538482e0c0a26ffd9bdc
marciojosebaldo <marciojosebaldo@gmail.com>
2023-09-25 21:03:07 -0300

    Add files via upload

--- A_ChamaMed/A_ChamaMed.ino
+++ A_ChamaMed/A_ChamaMed.ino
@@ -1,40 +1,63 @@
-void setup() {
+  int bipe = 12; // pino exclusivo para o bipe
+  int led = 13; // pino exclusivo para o LED
+
+  const int jgPortas1 = 10;
+  const int jgPortas2 = 32;
+
+  bool portas1[10];
+  bool portas2[32];
+
+  void setup() {
 
   Serial.begin(9600);
   
-  bipe = 12; // pino exclusivo para o bipe
-  led = 13; // pino exclusivo para o LED
-  
   pinMode(led, OUTPUT);
-  pinode(bipe, OUTPUT);
-  
-  digitalWrite(led, low);a
-  digitalWrite(bipe, low);
 
-  for (int i = 2; i <= 11; i++) {
-    pinMode(porta[i], INPUT);
+  for (int i = 0; i < sizeof(jgPortas1); i++) {
+    pinMode(portas1[i], INPUT);
+  }  
+
+  for (int j = 0; j < sizeof(jgPortas2); j++) {
+    pinMode(portas2[j], INPUT);
   }
-
-  for (int j = 22; j <= 53; j++) {
-    pinMode(porta[j], INPUT);
-  }
-
 }
 
 void loop() {
 
   if(Serial.available()) {
-    for (int i = 2; i <= 12; i++) {
-      if(porta[i] == HIGH) {
-        bool porta[i] = Serial.readBool();
-        
+    for (int i = 0; i <= sizeof(jgPortas1); i++) {
+      if(portas1[i] == 1) {
+        portas1[i] = Serial.read() == 1;
+        digitalWrite(led, high);
+        delay(200);
+        digitalWrite(led, low);
+        delay(200);
+
+        tone(bipe, 800);
+        delay(200);
+        noTone(bipe);
+        delay(200);
+      } else {
+        digitalWrite(led, low);
+        noTone(bipe);
       }
     }
 
-    for (int j = 22; j <= 53; j++) {
-      if(porta[j] == HIGH) {
-        bool porta[i] = Serial.readBool();
+    for (int j = 0; j <= sizeof(jgPortas2); j++) {
+      if(portas2[j] == 1) {
+        portas2[i] = Serial.read() == 1;
+        digitalWrite(led, high);
+        delay(200);
+        digitalWrite(led, low);
+        delay(200);
 
+        tone(bipe, 800);
+        delay(200);
+        noTone(bipe);
+        delay(200);
+    } else {
+        digitalWrite(led, low);
+        noTone(bipe);
     }
   }
 }
